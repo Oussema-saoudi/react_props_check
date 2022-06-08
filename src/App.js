55010558e089f33b8477b3af2ccd70db8d2179bf
Oussema-saoudi <65013740+Oussema-saoudi@users.noreply.github.com>
2022-06-08 12:01:52 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Profile from "./profile/Profile";
 import Ousama from "./img_saoudi.jpg";
-function handleName() {
+const HandleName = (props) => {
   return alert("Hello! the profile user is Ousama Saoudi");
-}
+};
 
 function App() {
   return (
     <div style={{ color: "aqua", textAlign: "center" }}>
+      <HandleName />
       <Profile
         fullName="Ousama Saoudi"
         bio="Currently  looking for a new opportunity with a first experience as a junior embedded software engineer at sagemcom software and technologies working in the Gateway department I think my profile would fit perfectly with your recent job requirements.
@@ -16,7 +17,6 @@ function App() {
         profession="Industrial electronics engineer"
       >
         <img width={400} height={300} src={Ousama} alt="imageinSource" />
-        {handleName()}
       </Profile>
     </div>
   );
